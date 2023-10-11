3110c6491ba33ba88b4525304c5df420c37dce82
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-11 17:18:34 -0600

    Components Initial Template Complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+
+import { Actors, MovieInformation, Movies, Navbar, Profile } from './';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+        <CssBaseline />
+        <Navbar />
+        <main>
+            <Switch>
+                <Route exact path='/movie/:id'>
+                    <MovieInformation />
+                </Route>
+                <Route exact path='/actors/:id'>
+                    <Actors />
+                </Route>
+                <Route exact path='/'>
+                    <Movies />
+                </Route>
+                <Route exact path='/profile/:id'>
+                    <Profile />
+                </Route>
+            </Switch>
+        </main>
+    </div>
   )
 }
 
