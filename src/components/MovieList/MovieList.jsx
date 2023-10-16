d239194cbb3a3d3e83b263c56ea33ef327eb9166
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-16 17:29:10 -0600

    MovieList.jsx Complete Component Done

---
+++ src/components/MovieList/MovieList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import { Movie } from '..';
+
+import useStyles from './styles';
+
+const MovieList = ({ movies }) => {
+
+    const classes = useStyles();
+
+  return (
+    <Grid container className={classes.moviesContainer}>
+        {movies.results.map((movie, index) => (
+            <Movie key={index} movie={movie} index={index} />
+        ))}
+    </Grid>
+  )
+}
+
+export default MovieList
