588b107a148aeed00f2099d6ce686be44ef3dee4
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-18 11:24:55 -0600

    Genre or Category Fetching Querys Functionality Added

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -7,13 +7,33 @@ export const tmdbApi = createApi({
     reducerPath: 'tmdbApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
     endpoints: (builder) => ({
+        //* Get Genres
+        getGenres: builder.query({
+            query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+        }),
+
         //* Get Movies by [Type]
         getMovies: builder.query({
-            query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+            query: ({ genreIdOrCategoryName, page }) => {
+
+                //* Get Movies By Category
+                if(genreIdOrCategoryName && typeof genreIdOrCategoryName === 'string'){
+                    return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`;
+                };
+            
+                //* Get Movies By Genre
+                if(genreIdOrCategoryName && typeof genreIdOrCategoryName === 'number'){
+                    return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`;
+                };
+
+                //* Get Popular Movies
+                return `movie/popular?page=${page}&api_key=${tmdbApiKey}`;
+            },
         }),
     }),
 });
 
 export const {
-    useGetMoviesQuery
+    useGetMoviesQuery,
+    useGetGenresQuery,
 } = tmdbApi;
