7c14a78cedf184d57349d533ba530bbeecc1ef91
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-19 12:47:48 -0600

    auth.js and utilities Files Update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Typography, Button, Box } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 
 const Profile = () => {
+
+  const { user } = useSelector((state) => state.user);
+
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = '/';
+  };
+
+  const favoriteMovies = [];
+
   return (
-    <div>Profile</div>
+    <Box>
+      <Box display='flex' justifyContent='space-between'>
+
+        <Typography variant='h4' gutterBottom >
+          My Profile
+        </Typography>
+
+        <Button color='inherit' onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+
+      </Box>
+
+      {!favoriteMovies.length
+        ? <Typography variant='h5'>Add favorites or watchlist some movies to see them here!!</Typography>
+        : <Box>
+          FAVORITE MOVIES
+        </Box>
+      }
+
+    </Box>
   )
 }
 
