cce9a996176a64b881c364bf73fb351f286f6d55
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-20 14:29:58 -0600

    Actors.jsx Complete Component Done

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -1,8 +1,83 @@
-import React from 'react'
+import React, { useReact } from 'react';
+import { Box, Button, CircularProgress, Grid, Typography } from '@mui/material';
+import { useHistory, useParams } from 'react-router-dom';
+import { ArrowBack } from '@mui/icons-material';
+import { useGetActorsDetailsQuery, useGetMoviesByActorIdQuery } from '../../services/TMDB';
+import useStyles from './styles';
+import { MovieList } from '..';
 
 const Actors = () => {
+
+  const page = 1;
+  const classes = useStyles();
+  const { id } = useParams();
+  const history = useHistory();
+  const { data, isFetching, error } = useGetActorsDetailsQuery(id);
+  const { data: movies } = useGetMoviesByActorIdQuery({ id, page });
+
+  if(isFetching){
+    return (
+      <Box display='flex' justifyContent='center'>
+        <CircularProgress size='8rem'/>
+      </Box>
+    )
+  };
+
+  if(error){
+    return (
+      <Box display='flex' justifyContent='center' alignItems='center'>
+        <Button startIcon={<ArrowBack />} onClick={() => history.goBack()} color='primary'>
+          Go Back
+        </Button>
+      </Box>
+    )
+  };
+
   return (
-    <div>Actors</div>
+    <>
+      <Grid container spacing={3}>
+        <Grid item lg={5} xl={4}>
+          <img 
+            className={classes.image} 
+            src={`https://image.tmdb.org/t/p/w780/${data?.profile_path}`} 
+            alt={data.name}
+          />
+        </Grid>
+
+        <Grid item lg={7}d xl={8} styles={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+          <Typography variant='h2' gutterBottom>
+            {data?.name}
+          </Typography>
+          
+          <Typography variant='h5' gutterBottom>
+            Born: {new Date(data?.birthday).toDateString()}
+          </Typography>
+
+          <Typography variant='body1' gutterBottom align='justify' paragraph>
+            {data?.biography || 'Sorry, no biography yet...'}
+          </Typography>
+
+          <Box marginTop='2rem' display='flex' justifyContent='space-around'>
+            <Button variant='contained' color='primary' target='_blank' href={`https://www.imdb.com/name/${data?.imdb_id}`}>
+              IMDB
+            </Button>
+
+            <Button startIcon={<ArrowBack />} onClick={() => history.goBack()} color='primary'>
+              Back
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+
+      <Box margin='2rem 0'>
+        <Typography variant='h2' gutterBottom align='center'>
+          Movies
+        </Typography>
+        {movies && (
+          <MovieList movies={movies} numberOfMovies={12}/>
+        )}
+      </Box>
+    </>
   )
 }
 
