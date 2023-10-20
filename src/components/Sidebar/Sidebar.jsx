34146126a0ecb7ab38b10661bd10ff80d57c9ba2
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-20 13:19:30 -0600

    Sidebar.jsx Component Update

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,7 +7,7 @@ import { useGetGenresQuery } from '../../services/TMDB';
 import genreIcons from '../../assets/genres';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { genreOrCategory, selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 
 const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
 const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
