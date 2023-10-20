6d665041dfd2d80a7cf9cee3c8d128d57c05186a
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-20 16:24:00 -0600

    Movies.jsx Component Update

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -5,12 +5,16 @@ import { useSelector } from 'react-redux';
 import { useGetMoviesQuery } from '../../services/TMDB';
 import { MovieList } from '..';
 import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+import { Pagination } from '..';
 
 const Movies = () => {
 
   const [page, setPage] = useState(1);
   const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.currentGenreOrCategory);
   const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
+  const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
+
+  const numberOfMovies = lg ? 16 : 18;
 
   // Loading
   if(isFetching){
@@ -39,7 +43,8 @@ const Movies = () => {
 
   return (
     <div>
-      <MovieList movies ={data} />
+      <MovieList movies ={data} numberOfMovies={numberOfMovies}/>
+      <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages}/>
     </div>
   )
 }
