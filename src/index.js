4a4257eb54bb3453fcf6d4ce98485ad546e1d7c5
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-21 18:10:13 -0600

    App.jsx Component Update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Provider } from "react-redux";
 
 import App from "./components/App";
 import store from "./app/store";
+import { Provider } from "react-redux";
+import './index.css';
 
 const theme = createTheme({});
 
