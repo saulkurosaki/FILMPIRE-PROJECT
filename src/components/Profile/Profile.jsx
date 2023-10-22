b290ed66a4bd1e4886ffa5d6497ab6207102ed53
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-22 11:54:18 -0600

    Profile.jsx Component Update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,18 +2,25 @@ import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Typography, Button, Box } from '@mui/material';
 import { ExitToApp } from '@mui/icons-material';
+import { useGetListQuery } from '../../services/TMDB';
+import { RatedCards } from '..';
 
 const Profile = () => {
 
   const { user } = useSelector((state) => state.user);
+  const { data: favoriteMovies, refetch: refetchFavorites } = useGetListQuery({ listName: 'favorite/movies', accountId: user.id, sessionId: localStorage.getItem('session_id'), page: 1 });
+  const { data: watchlistMovies, refetch: refetchWatchlisted } = useGetListQuery({ listName: 'watchlist/movies', accountId: user.id, sessionId: localStorage.getItem('session_id'), page: 1 });
+
+  useEffect(() => {
+    refetchFavorites();
+    refetchWatchlisted();
+  }, []);
 
   const logout = () => {
     localStorage.clear();
 
     window.location.href = '/';
   };
-
-  const favoriteMovies = [];
 
   return (
     <Box>
@@ -29,10 +36,11 @@ const Profile = () => {
 
       </Box>
 
-      {!favoriteMovies.length
+      {!favoriteMovies?.results?.length && !watchlistMovies?.results?.length
         ? <Typography variant='h5'>Add favorites or watchlist some movies to see them here!!</Typography>
         : <Box>
-          FAVORITE MOVIES
+          <RatedCards title='Favorite Movies' data={favoriteMovies} />
+          <RatedCards title='Watchlist' data={watchlistMovies} />
         </Box>
       }
 
