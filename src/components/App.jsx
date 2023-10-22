de857b986d26bb7226fa55d6137c320542e2d072
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-22 15:34:53 -0600

    App.jsx Final Component Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 
 import useStyles from './styles';
+import useAlan from './Alan';
 
 import { Actors, MovieInformation, Movies, Navbar, Profile } from './';
 
 const App = () => {
 
     const classes = useStyles();
+    const alanBtnContainer = useRef();
+    
+    useAlan();
 
   return (
     <div className={classes.root}>
@@ -33,6 +37,7 @@ const App = () => {
                 </Route>
             </Switch>
         </main>
+        <div ref={alanBtnContainer}/>
     </div>
   )
 }
