ef0ad19876c1e13ba250519d68683254ed5e3cdf
Saul Paredes <saulkurosaki13@gmail.com>
2023-10-22 15:36:00 -0600

    Movies.jsx Final Component Update;

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
-import { MovieList } from '..';
-import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
-import { Pagination } from '..';
+import { FeaturedMovie, Pagination, MovieList } from '..';
 
 const Movies = () => {
 
@@ -14,7 +12,7 @@ const Movies = () => {
   const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
   const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
 
-  const numberOfMovies = lg ? 16 : 18;
+  const numberOfMovies = lg ? 17 : 19;
 
   // Loading
   if(isFetching){
@@ -43,7 +41,8 @@ const Movies = () => {
 
   return (
     <div>
-      <MovieList movies ={data} numberOfMovies={numberOfMovies}/>
+      <FeaturedMovie movie={data.results[0]} />
+      <MovieList movies ={data} numberOfMovies={numberOfMovies} excludeFirst/>
       <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages}/>
     </div>
   )
